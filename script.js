1c8a299dd2dfa79a9fc9a009629914b020f1588f
rmaklo <rayan.makhloufi94@gmail.com>
2025-05-26 17:36:05 +0200

    update 26 - 05 - 2025

--- script.js
+++ script.js
@@ -10,50 +10,54 @@ function getData() {
         // Traitez les données comme vous le souhaitez
         console.log('Données récupérées du fichier JSON :', data);
         /// ON ECRIT LE CODE ICI ! 
+        let body = document.querySelector('body')
         let journal = data.journal;
         let main = document.querySelector('main')
         console.log(journal);
-        
-        // 1 : afficher dans la nav les thèmes
-        // Sélection de la barre <nav>
-        let navContainer = document.getElementById('navigation')
 
-        // Dans la liste menu <ul>, intégrer les thèmes cliquables <li>.
-        let menu = document.getElementById('menu')
+        // 1 : Barre de navigation
+        let navigation = document.getElementById('navigation')
 
+        // thèmes
         journal.themes.forEach(theme => {
+          let menu = document.getElementById('menu')
           let listTheme = document.createElement('li')
           let lienTheme = document.createElement('a')
-          lienTheme.href = "#"
-          listTheme.appendChild(lienTheme)
           lienTheme.textContent = theme.nom
-          navContainer.appendChild(listTheme)
-        });
+          lienTheme.href = "#"
 
-        //Bouton s'abonner
+          listTheme.appendChild(lienTheme)
+          menu.appendChild(listTheme)
+          
+        });
+        navigation.appendChild(menu)
+
+        //Bouton "s'abonner"
         let boutonPrimaire = document.createElement('a')
         boutonPrimaire.className = "bouton-primaire"
         boutonPrimaire.textContent = "S'abonner"
         boutonPrimaire.href = "#"
-        navContainer.appendChild(boutonPrimaire)
 
         // Photo de profil de l'utilisateur
-        let photoProfil = document.createElement('div')
-        let lienProfil = document.createElement('a')
-        lienProfil.href = "#"
+        let photoProfil = document.createElement('a')
+        photoProfil.href = "#"
         photoProfil.className = "photo-profil"
 
-        let image = document.createElement('img')
-        image.src = "images/djibril.png"
-        image.alt = "photo de profil"
+        let imgProfil = document.createElement('img')
+        imgProfil.className='img-profil'
+        imgProfil.src = "images/djibril.png"
+        imgProfil.alt = "photo de profil"
 
-        lienProfil.appendChild(image)
-        photoProfil.appendChild(lienProfil)
-        navContainer.appendChild(photoProfil)
+        photoProfil.appendChild(imgProfil)
 
-        // Header
+        let divPhotoBouton = document.createElement('div')
+        divPhotoBouton.className = "photo-bouton"
+        divPhotoBouton.append(boutonPrimaire, photoProfil)
+        navigation.appendChild(divPhotoBouton)
 
-        let headerContainer = document.createElement('header')
+        // 2 : Header
+
+        let headerContainer = document.querySelector('header')
         let divTitre = document.createElement('div')
         divTitre.className = "titre"
 
@@ -66,15 +70,154 @@ function getData() {
         headerContainer.appendChild(divTitre)
         main.appendChild(headerContainer)
 
-        console.log(headerContainer);
+        let divArticle = document.createElement('div')
+        divArticle.className = 'divArticle'
+
+        journal.themes.forEach(theme => {
+          let articleTheme = document.createElement('article')
+          articleTheme.className = "theme"
+          divArticle.appendChild(articleTheme)
+
+          let titreTheme = document.createElement('h2')
+          titreTheme.textContent = theme.nom
+
+          let texteTheme = document.createElement('p')
+          texteTheme.className = "texte-theme"
+          texteTheme.textContent = theme.description
+
+          articleTheme.append(titreTheme, texteTheme)
+          divArticle.appendChild(articleTheme)
+          headerContainer.appendChild(divArticle)
+          
+        })
+
+        // 3 : Section "actu à la une"
+
+        let headlineSection = document.getElementById('headline')
+
+        let imgHeadline = document.createElement('img')
+        imgHeadline.className='img-headline'
+        imgHeadline.src = journal.articlePrincipal.image
+        imgHeadline.alt = "article principal"
+
+        let articleHeadline = document.createElement('article')
+        articleHeadline.className = 'headline'
+
+        let divHeadlineTitle = document.createElement('div')
+        divHeadlineTitle.className = 'title'
+
+        let titleHeadline = document.createElement('h2')
+        titleHeadline.textContent = journal.articlePrincipal.titre
         
+        let themeHeadline = document.createElement('h3')
+        themeHeadline.textContent = journal.articlePrincipal.theme
+
+        divHeadlineTitle.append(titleHeadline, themeHeadline)
+
+        let texteHeadline = document.createElement('p')
+        texteHeadline.textContent = journal.articlePrincipal.description
+
+        let boutonMore1 = document.createElement('a')
+        boutonMore1.className = "bouton-primaire"
+        boutonMore1.textContent = "Lire l'article"
+        boutonMore1.href = "#"
+
+
+        articleHeadline.append(divHeadlineTitle, texteHeadline, boutonMore1)
+        headlineSection.append(imgHeadline, articleHeadline)
+
+        // 4 : Section des articles
+
+        let newsSection = document.getElementById('news')
+
+        let divNewsContainer = document.createElement('div')
+        divNewsContainer.className = 'news-container'
+
+        // boucle présentation des articles
+        journal.articles.forEach(article => {
+          ajouterArticle (article, divNewsContainer)
+        });
+
+        function ajouterArticle (article, divNewsContainer) {
+          let articleNews = document.createElement('article')
+          articleNews.className = 'news'
+
+          // Image d'illustration
+          let imgNews = document.createElement('img')
+          imgNews.className = 'img-news'
+          imgNews.src = article.image
+          imgNews.alt = "Image Article"
+          
+          // Texte News
+          let divNewsTexte = document.createElement('div')
+          divNewsTexte.className = 'news-texte'
+
+          let divNewsTitle = document.createElement('div')
+          divNewsTitle.className = 'news-title'
+
+          let titleNews = document.createElement('h2')
+          titleNews.textContent = article.titre
+          
+          let themeNews = document.createElement('h3')
+          themeNews.textContent = article.theme
+
+          divNewsTitle.append(titleNews, themeNews)
+
+          let texteNews = document.createElement('p')
+          texteNews.textContent = article.description
+
+          boutonMore1 = document.createElement('a')
+          boutonMore1.className = "bouton-primaire"
+          boutonMore1.textContent = "Lire l'article"
+          boutonMore1.href = "#"
+
+          divNewsTexte.append(divNewsTitle, texteNews, boutonMore1)
+          articleNews.append(imgNews, divNewsTexte)
+          divNewsContainer.appendChild(articleNews)
+        }
 
         
+        newsSection.appendChild(divNewsContainer)
+
+        // 5 : Section journalistes
+
+        let teamSection = document.getElementById('team')
+
+        let divTeamContainer = document.createElement('div')
+        divTeamContainer.className = "team-container"
         
+        journal.auteurs.forEach(auteur => {
+          ajouterTeam (auteur, divTeamContainer)
+        })
 
+        function ajouterTeam (auteur, divTeamContainer) {
+          let articleTeam = document.createElement('article')
+          articleTeam.className = "team"
 
+          let divTeamTexte = document.createElement('div')
+          divTeamTexte.className = "team-texte"
 
+          let imgTeam = document.createElement('img')
+            imgTeam.src = auteur.image
+            imgTeam.alt = "photo auteur"
+
+          let nameTeam = document.createElement('h2')
+            nameTeam.textContent = auteur.prenom
+
+          let expTeam = document.createElement('h3')
+            expTeam.textContent = auteur.typeExperience
+
+          let descriptionTeam = document.createElement('p')
+          descriptionTeam.textContent = auteur.presentation
+
+          divTeamTexte.append(nameTeam, expTeam, descriptionTeam)
+          articleTeam.append(imgTeam, divTeamTexte)
+          divTeamContainer.appendChild(articleTeam)
+        }
       
+        teamSection.appendChild(divTeamContainer)
+        
+        main.append(headerContainer, headlineSection, newsSection, teamSection)
 
        /// FIN DU CODE
      })
